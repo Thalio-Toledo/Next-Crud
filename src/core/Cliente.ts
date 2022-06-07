1ae572f60d9ac09d57027f81110bab474101b103
thalio-toledo <thalio.speed@hotmail.com>
2022-06-06 22:20:22 -0300

    Primeiro commit do projeto crud

---
+++ src/core/Cliente.ts
@@ -0,0 +1,9 @@
+export default class Cliente{
+  private id: string
+  private nome: string
+  private idade: number
+
+  constructor(nome:string, idade: number, id:string = null ){
+    
+  }
+}
---
+++ src/pages/index.tsx
@@ -0,0 +1,15 @@
+import Layout from "../components/Layout"
+
+export default function Home() {
+  return (
+    <div className={`
+    flex h-screen justify-center items-center
+    bg-gradient-to-r from-purple-500 to-blue-600
+    `}>
+     <Layout titulo="Cadastro Simples">
+       <span>Conteudo</span>
+     </Layout>
+    
+    </div>
+  )
+}
